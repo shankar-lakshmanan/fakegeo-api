c341f65a15ed6d442aa9a13f06e470c7b334bf22
Shankar Lakshmanan <sanks87@gmail.com>
2024-12-22 00:18:32 -0600

    Create version 1.0.0

--- fakegeo-web/docusaurus.config.ts
+++ fakegeo-web/docusaurus.config.ts
@@ -122,6 +122,10 @@ const config: Config = {
         {to: '/blog', label: 'Blog', position: 'left'},
         {to: '/api', label: 'API', position: 'left'},
         {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
+        {
           href: 'https://github.com/fakegeo',
           label: 'GitHub',
           position: 'right',
